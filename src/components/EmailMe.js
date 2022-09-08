291f00aaf6607727ef56f5494abd83cd39084ccb
unknown <robbie6400@gmail.com>
2022-09-08 10:40:47 -0400

    minor updates

--- src/components/EmailMe.js
+++ src/components/EmailMe.js
@@ -3,16 +3,18 @@ import '../css/EmailMe.css';
 
 export default function EmailMe() {
   return (
-    <>    
-        <div className="yellowBox">
-            <h2>Send me an email:</h2>
-            <form action="mailto:skyler.greensboro@gmail.com" method="post" encType="text/plain">
-                <input type="text" name="Message from fan: " size="50" />
-                <input type="reset" value="Clear" />
-                <input type="submit" value="Book Now" />
-            </form>
-            <br />
+    <div className="emailForm">
+      <h2>Send me an email:</h2>
+      <form action="mailto:robbie6400@gmail.com" method="post" encType="text/plain">
+        <div className='formInput'>
+          <input type="text" name="MESSAGE FROM SWL: " size="50" />
         </div>
-    </>
+        <p>Please leave your name, way of contact, location and venue name.</p>
+        <div className='formButtons'>
+          <input type="reset" value="Clear" />
+          <input type="submit" value="Book Now" />
+        </div>
+      </form>
+    </div>
   )
 }
